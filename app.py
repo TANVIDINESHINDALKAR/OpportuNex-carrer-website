7041af8732e8d6b68e97dd0c5abd74e5b32ca2b1
Tanishq Doke <112191079+TanishqDoke@users.noreply.github.com>
2024-09-12 16:26:30 +0000

    Database

--- app.py
+++ app.py
@@ -1,47 +1,25 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
+import pymysql
 
 app = Flask(__name__)
-
-JOBS = [
-    {
-        'id': 1,
-        'title': 'Data Analyst',
-        'location': 'Bengaluru, India',
-        'salary': 'Rs. 10,00,000'
-    },
-    {
-        'id': 2,
-        'title': 'Data Scientist',
-        'location': 'Delhi, India',
-        'salary': 'Rs. 15,00,000'
-    },
-    {
-        'id': 3,
-        'title': 'Frontend Engineer',
-        'location': 'Remote',
-        # 'salary': 'Rs. 12,00,000'
-    },
-    {
-        'id': 3,
-        'title': 'Backend Engineer',
-        'location': 'San Franscisco, USA',
-        'salary': '$120,000'
-    }
-]
 
 
 #html endpoint, html route
 @app.route("/")
 def hello_world():
-  return render_template('home.html', jobs=JOBS, company_name="OpportuNex")
+    jobs = load_jobs_from_db()
+
+    return render_template('home.html', jobs=jobs, company_name="OpportuNex")
 
 
 #json endpoint, api route
 #we can also access the info in the form of json and not only html
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+    jobs = load_jobs_from_db()
+    return jsonify(jobs)
 
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080)
---
+++ database.py
@@ -0,0 +1,39 @@
+import pymysql
+import os
+
+
+def load_jobs_from_db():
+    try:
+        # Establish the connection
+        connection = pymysql.connect(
+            charset="utf8mb4",
+            connect_timeout=10,
+            cursorclass=pymysql.cursors.DictCursor,
+            db=os.environ['DB_NAME'],
+            host=os.environ['DB_HOST'],
+            password=os.environ['DB_PASSWORD'],
+            read_timeout=10,
+            port=26415,
+            user=os.environ['DB_USER'],
+            write_timeout=10,
+        )
+
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM jobs")
+            result_all = cursor.fetchall()
+
+        connection.close()  # Always close the connection
+
+        # Process results
+        jobs = []
+        for row in result_all:
+            jobs.append(dict(row))
+
+        return jobs
+
+    except pymysql.Error as e:
+        print(f"Error connecting to MySQL: {e}")
+        return []  # Return an empty list or handle the error appropriately
+    except Exception as e:
+        print(f"General error: {e}")
+        return []  # Return an empty list or handle the error appropriately
