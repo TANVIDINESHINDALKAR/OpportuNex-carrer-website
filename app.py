7dc13d9de9e59e1e3081dbd3fe4ec8350dec2432
Tanishq Doke <112191079+TanishqDoke@users.noreply.github.com>
2024-09-14 12:21:01 +0000

    Added API

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, json, render_template, jsonify, request
 from database import load_jobs_from_db, load_jobs_from_db_id, add_application_to_db
 
 app = Flask(__name__)
@@ -28,13 +28,21 @@ def show_job(id):
 
     return render_template('jobpage.html', job=job)
 
+
+@app.route("/api/jobs/<id>")
+def show_job_json(id):
+    job = load_jobs_from_db_id(id)
+    return jsonify(job)
+
+
 @app.route("/job/<id>/apply", methods=['POST'])
 def apply_to_job(id):
     data = request.form
-    job=load_jobs_from_db_id(id)
-    add_application_to_db(id,data)
-    return render_template('application_submitted.html', application=data,job=job)
-
+    job = load_jobs_from_db_id(id)
+    add_application_to_db(id, data)
+    return render_template('application_submitted.html',
+                           application=data,
+                           job=job)
 
 
 if __name__ == "__main__":
